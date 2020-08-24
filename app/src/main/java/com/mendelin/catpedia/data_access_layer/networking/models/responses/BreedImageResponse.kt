12ba24a22ddb5ced52d86adb0bed060b32601a54
Madalin Balint <madalin_bacau@yahoo.com>
2020-08-24 18:09:20 +0300

    - added network models for breed info and image

---
+++ app/src/main/java/com/mendelin/catpedia/data_access_layer/networking/models/responses/BreedImageResponse.kt
@@ -0,0 +1,12 @@
+package com.mendelin.catpedia.data_access_layer.networking.models.responses
+
+import androidx.annotation.Keep
+
+@Keep
+class BreedImageResponse {
+    val breeds: List<BreedImageResponse>? = null
+    val id: String? = null
+    val url: String? = null
+    val width: Int? = null
+    val height: Int? = null
+}
---
+++ app/src/main/java/com/mendelin/catpedia/data_access_layer/networking/models/responses/BreedInfoResponse.kt
@@ -0,0 +1,43 @@
+package com.mendelin.catpedia.data_access_layer.networking.models.responses
+
+import androidx.annotation.Keep
+
+@Keep
+class BreedInfoResponse {
+    val weight: Map<String, String>? = null
+    val id: String? = null
+    val name: String? = null
+    val cfa_url: String? = null
+    val vetstreet_url: String? = null
+    val vcahospitals_url: String? = null
+    val temperament: String? = null
+    val origin: String? = null
+    val country_codes: String? = null
+    val country_code: String? = null
+    val description: String? = null
+    val life_span: String? = null
+    val indoor: Int? = null
+    val lap: Int? = null
+    val alt_names: String? = null
+    val adaptability: Int? = null
+    val affection_level: Int? = null
+    val child_friendly: Int? = null
+    val dog_friendly: Int? = null
+    val energy_level: Int? = null
+    val grooming: Int? = null
+    val health_issues: Int? = null
+    val intelligence: Int? = null
+    val shedding_level: Int? = null
+    val social_needs: Int? = null
+    val stranger_friendly: Int? = null
+    val vocalisation: Int? = null
+    val experimental: Int? = null
+    val hairless: Int? = null
+    val natural: Int? = null
+    val rare: Int? = null
+    val rex: Int? = null
+    val suppressed_tail: Int? = null
+    val short_legs: Int? = null
+    val wikipedia_url: String? = null
+    val hypoallergenic: Int? = null
+}
