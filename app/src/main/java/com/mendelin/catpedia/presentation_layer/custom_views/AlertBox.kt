413fb2765ce5a4dd140e00e53674351cf07b71d8
Madalin Balint <madalin_bacau@yahoo.com>
2020-08-25 22:28:29 +0300

    - added class for alert dialogs

---
+++ app/src/main/java/com/mendelin/catpedia/presentation_layer/custom_views/AlertBox.kt
@@ -0,0 +1,37 @@
+package com.mendelin.catpedia.presentation_layer.custom_views
+
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class AlertBox(val context: Context) : AppCompatActivity() {
+    private var listenerPositiveButton: DialogInterface.OnClickListener? = null
+    private var listenerNegativeButton: DialogInterface.OnClickListener? = null
+
+    fun setPositiveButtonListener(listener: DialogInterface.OnClickListener) {
+        listenerPositiveButton = listener
+    }
+
+    fun setNegativeButtonListener(listener: DialogInterface.OnClickListener) {
+        listenerNegativeButton = listener
+    }
+
+    fun showAlert(title: String, message: String, btnPositive: String?, btnNegative: String?) {
+        val alertDialogBuilder = AlertDialog.Builder(context)
+
+        alertDialogBuilder.setTitle(title)
+        alertDialogBuilder.setMessage(message)
+
+        if (listenerPositiveButton != null)
+            alertDialogBuilder.setPositiveButton(btnPositive, listenerPositiveButton)
+
+        if (listenerNegativeButton != null)
+            alertDialogBuilder.setNegativeButton(btnNegative, listenerNegativeButton)
+
+        val alertDialog = alertDialogBuilder.create()
+        if (!alertDialog.isShowing) {
+            alertDialog.show()
+        }
+    }
+}
