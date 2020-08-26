777dbfaa2add3ce3be2c14a5ddf516b4f5d28261
Madalin Balint <madalin_bacau@yahoo.com>
2020-08-26 12:58:05 +0300

    - added models for login

---
+++ app/src/main/java/com/mendelin/catpedia/presentation_layer/activities/welcome_screen/bussiness_logic/models/UserObject.kt
@@ -0,0 +1,7 @@
+package com.mendelin.catpedia.presentation_layer.activities.welcome_screen.bussiness_logic.models
+
+data class UserObject (
+    val user_name: String? = null,
+    val user_email: String? = null,
+    val auth_token: String? = null,
+)
