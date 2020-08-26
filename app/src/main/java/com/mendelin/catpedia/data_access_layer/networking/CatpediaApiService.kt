c27de2587524692ee4ead962c13473e74c61f6f2
Madalin Balint <madalin_bacau@yahoo.com>
2020-08-26 13:00:25 +0300

    - code refactoring

--- app/src/main/java/com/mendelin/catpedia/data_access_layer/networking/CatpediaApiService.kt
+++ app/src/main/java/com/mendelin/catpedia/data_access_layer/networking/CatpediaApiService.kt
@@ -2,7 +2,7 @@ package com.mendelin.catpedia.data_access_layer.networking
 
 import com.mendelin.catpedia.data_access_layer.networking.models.responses.BreedImageResponse
 import com.mendelin.catpedia.data_access_layer.networking.models.responses.BreedInfoResponse
-import retrofit2.Call
+import com.mendelin.catpedia.presentation_layer.activities.welcome_screen.bussiness_logic.models.LoginResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,5 +11,9 @@ interface CatpediaApiService {
     suspend fun getListOfCatBreeds(): List<BreedInfoResponse>
 
     @GET("v1/images/search")
-    suspend fun getBreedImage(@Query("breed_id") breedId: String): MutableList<BreedImageResponse>
+    suspend fun getBreedImage(@Query("breed_id") breedId: String): List<BreedImageResponse>
+
+    @GET("v1/login.php")
+    suspend fun loginUser(@Query("username") username: String,
+                          @Query("password") password: String): LoginResponse
 }
--- app/src/main/java/com/mendelin/catpedia/data_access_layer/repository/LocalRepository.kt
+++ app/src/main/java/com/mendelin/catpedia/data_access_layer/repository/LocalRepository.kt
@@ -1,19 +1,23 @@
 package com.mendelin.catpedia.data_access_layer.repository
 
 import androidx.annotation.Keep
-import com.google.gson.GsonBuilder
 import com.mendelin.catpedia.business_layer.CatpediaApplication
 import com.mendelin.catpedia.common.ResourceUtils
-
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 @Keep
 class LocalRepository<T>(private val id: Int, private val classOfT: Class<T>) : Repository<T> {
-    private val DATE_TIME_ZONE_XML_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     override fun readData(): T? {
         val file = CatpediaApplication.getInstance().resources.openRawResource(id)
         val strData = ResourceUtils.readTextFile(file)
-        val gson = GsonBuilder().setDateFormat(DATE_TIME_ZONE_XML_FORMAT).create()
-        return gson.fromJson(strData, classOfT)
+
+        val adapter: JsonAdapter<T> = moshi.adapter(classOfT)
+        return adapter.fromJson(strData)
     }
 }
