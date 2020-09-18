e13292d14bd35c504bc3ffcf45a76f79f9eba87e
Madalin Balint <madalin_bacau@yahoo.com>
2020-09-18 17:03:56 +0300

    - removed unused file

--- app/src/main/java/com/mendelin/catpedia/rest_service/CatpediaApi.kt
+++
@@ -1,9 +0,0 @@
-package com.mendelin.catpedia.rest_service
-
-import com.mendelin.catpedia.retrofit.RetrofitService
-
-object CatpediaApi {
-    val service: CatpediaApiService by lazy {
-        RetrofitService().createService(CatpediaApiService::class.java)
-    }
-}
