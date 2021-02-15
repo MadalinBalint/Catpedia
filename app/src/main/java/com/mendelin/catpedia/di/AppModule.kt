2eae365e02d66b4e1c6dc29013fa53b7bd6fb4d4
Madalin Balint <madalin.balint@fortech.ro>
2021-02-15 15:28:19 +0200

    added support to automatically retry the network call for 3 times if you receive a server error (5xx) with a 1000ms pause between the calls

--- app/src/main/java/com/mendelin/catpedia/di/AppModule.kt
+++ app/src/main/java/com/mendelin/catpedia/di/AppModule.kt
@@ -11,10 +11,16 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
 class AppModule {
+    companion object {
+        const val TRY_COUNT = 3
+        const val TRY_PAUSE_BETWEEN = 1000L
+    }
+
     @Singleton
     @Provides
     fun provideGsonInstance(): Gson {
@@ -31,7 +37,25 @@ class AppModule {
                 Interceptor { chain ->
                     val builder = chain.request().newBuilder()
                     builder.header(BuildConfig.API_HEADER, BuildConfig.API_KEY)
-                    return@Interceptor chain.proceed(builder.build())
+
+                    var response = chain.proceed(builder.build())
+
+                    /* Automatically retry the call for N times if you receive a server error (5xx) */
+                    var tryCount = 0
+                    while (response.code() in 500..599 && tryCount < TRY_COUNT) {
+                        try {
+                            response.close()
+                            Thread.sleep(TRY_PAUSE_BETWEEN)
+                            response = chain.proceed(builder.build())
+                            Timber.e("Request is not successful - $tryCount")
+                        } catch (e: Exception) {
+                            Timber.e(e.localizedMessage)
+                        } finally {
+                            tryCount++
+                        }
+                    }
+
+                    return@Interceptor response
                 }
             )
         }.build()
